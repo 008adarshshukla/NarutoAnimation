2a1972354294bbd9a9554704b5159abbc98a0c87
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-07-05 10:39:07 +0530

    Initial Commit

---
+++ NarutoAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NarutoAnimation
+//
+//  Created by Adarsh Shukla on 05/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NarutoAnimation/NarutoAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarutoAnimationApp.swift
+//  NarutoAnimation
+//
+//  Created by Adarsh Shukla on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NarutoAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
