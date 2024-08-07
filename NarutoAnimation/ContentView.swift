74c1068a0673416f5e9d40ca5d368686da74b1f0
adarsh <shuklaadarsh1932@gmail.com>
2024-08-08 02:30:24 +0530

    feat: made view as public

--- NarutoAnimation/ContentView.swift
+++ NarutoAnimation/ContentView.swift
@@ -12,14 +12,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+public struct NarutoContentView: View {
+    public var body: some View {
         NarutoView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NarutoContentView()
     }
 }
